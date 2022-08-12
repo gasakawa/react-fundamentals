af8aa8602df66db95dc8245b7b057a3bb16658ee
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2022-08-12 16:54:49 -0300

    chore: configure webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<h1>Olá mundo</h1>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require(`clean-webpack-plugin`);
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
