6a01d5e2e9959ab7a4c1073a46a443ce9597b6ac
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2022-08-16 12:55:38 -0300

    feat: styled components with props more thant one property

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -4,4 +4,5 @@ export const Container = styled.article`
   margin-bottom: 24px;
 
   opacity: ${(props) => (props.removed ? 0.3 : 1)};
+  color: ${(props) => (props.removed ? '#ff0000' : '#fff')};
 `;
